7b4fe3f1f6dce0f79ada548f99949efed20e1d26
longvibol <vibollong@gmail.com>
2025-07-09 08:44:22 +0700

    er

---
+++ spring boot softcode/LinkedListDemoApp.java
@@ -0,0 +1,54 @@
+package com.piseth.java.school;
+
+public class LinkedListDemoApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		LinkedListExample list = new LinkedListExample();
+		list.addAtEnd(30);
+		list.addAtEnd(20);
+		list.addAtEnd(40);
+		
+		list.addAtEnd(50);
+		list.addAtEnd(40);
+		list.addAtEnd(60);
+		list.addAtEnd(40);
+		
+		list.printList();
+		*/
+		/*
+		list.addAtBeginning(5);
+		list.printList();
+		
+		list.addAfter(20, 25);
+		list.printList();
+		
+		list.removeAtBeginning();
+		list.printList();
+		
+		list.removeAtEnd();
+		list.printList();
+		
+		list.removeByValue(20);
+		list.printList();
+		*/
+		
+		//30->20->10->null
+		//list.reverse();
+		//list.printList();
+		//list.findMiddle();
+		//list.findNthNodeFromEnd(2);
+		
+		//list.removeDuplicateUnorderedList();
+		//list.printList();
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
